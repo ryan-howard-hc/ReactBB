2345fa8e4b7167de5d5621cfce2020bcc3ba6a6b
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-11-30 17:54:42 -0500

    Found a functioning hexgrid

--- battle/src/App.js
+++ battle/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../src/BOARD/Hexboard.css';
 import HexGrid from '../src/BOARD/Hexgrid';
-import Hexagon from './BOARD/Hex';
+import HexBoard from '../src/BOARD/Hex';
 
 const App = () => {
   const imageUrl = 'https://opengameart.org/sites/default/files/grass_15.png'; 
 
   return (
     <div className="App">
-      <Hexagon/>
+      <HexBoard />
     </div>
   );
 };
