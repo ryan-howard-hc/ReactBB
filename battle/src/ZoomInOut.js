e00568bf9dd7ed5ff147d3fdb8ccd79a369ceef5
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-12-01 00:05:14 -0500

    found nice code to add dragging ability to map when zoomed in

--- battle/src/ZoomInOut.js
+++ battle/src/ZoomInOut.js
@@ -1,27 +1,62 @@
 // ZoomInOut.js
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const ZoomableContent = ({ children }) => {
-  const minZoomLevel = 0.5; //minimum zoom level
+  const minZoomLevel = 0.5; // minimum zoom level
   const [zoomLevel, setZoomLevel] = useState(1); // initial zoom level
+  const [isDragging, setIsDragging] = useState(false);
+  const [prevX, setPrevX] = useState(0);
+  const [prevY, setPrevY] = useState(0);
+  const contentRef = useRef(null);
 
   const zoomFactor = 0.005; // control the zoom rate
 
   const handleWheel = (event) => {
     event.preventDefault(); // prevents default scrolling behavior
 
-    // adjusts zoom level based on the scroll direction (up/down)
     setZoomLevel((prevZoomLevel) => {
-      const newZoomLevel = prevZoomLevel + event.deltaY * zoomFactor;
+      const newZoomLevel = prevZoomLevel - event.deltaY * zoomFactor;
       return Math.max(minZoomLevel, newZoomLevel); // Restrict zoom level to the minimum value
     });
+  };
+
+  const handleMouseDown = (event) => {
+    setIsDragging(true);
+    setPrevX(event.clientX);
+    setPrevY(event.clientY);
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!isDragging) return;
+
+    const deltaX = event.clientX - prevX;
+    const deltaY = event.clientY - prevY;
+
+    contentRef.current.scrollLeft -= deltaX;
+    contentRef.current.scrollTop -= deltaY;
+
+    setPrevX(event.clientX);
+    setPrevY(event.clientY);
   };
 
   return (
     <div
       id="zoomableContent"
-      style={{ transform: `scale(${zoomLevel})`, transformOrigin: 'top left', overflow: 'hidden' }}
+      style={{
+        transform: `scale(${zoomLevel})`,
+        transformOrigin: 'top left',
+        overflow: 'hidden',
+        cursor: isDragging ? 'grabbing' : 'grab'
+      }}
       onWheel={handleWheel}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseMove={handleMouseMove}
+      ref={contentRef}
     >
       {children}
     </div>
